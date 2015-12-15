c7abde3c60908faacee6b01aa9bdd6d259827438
Ocean WINCKLER <owinckle@e1r9p6.42.fr>
2015-12-15 16:18:50 +0100

    Working Itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,16 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: owinckle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/26 15:19:41 by owinckle          #+#    #+#             */
-/*   Updated: 2015/12/15 12:17:37 by owinckle         ###   ########.fr       */
+/*   Created: 2015/12/15 14:40:37 by owinckle          #+#    #+#             */
+/*   Updated: 2015/12/15 16:17:12 by owinckle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-static int	ft_countchars(int n)
+/*static int		numcheck(int n)
 {
-	int i;
+	int				i;
 
 	i = 0;
 	if (n == 0)
@@ -23,23 +24,23 @@ static int	ft_countchars(int n)
 		i++;
 	else
 	{
-		while (n > 0)
+		while (n > 10)
 		{
 			i++;
-			n /= 10;
+			n /=10;
 		}
-	}
+	}	
 	return (i);
 }
 
-char		*ft_itoa(int n)
+char			*ft_itoa(int n)
 {
 	int				i;
-	unsigned int	x;
-	int				sign;
 	char			*buf;
+	unsigned int 	x;
+	int 			sign;
 
-	buf = ft_strnew(ft_countchars(n));
+	buf = ft_strnew(numcheck(n));
 	if (buf == NULL)
 		return (NULL);
 	x = n;
@@ -58,4 +59,42 @@ char		*ft_itoa(int n)
 	buf[i] = '\0';
 	ft_strrev(buf);
 	return (buf);
+}*/
+
+static void	itoa_isnegative(int *n, int *negative)
+{
+	if (*n < 0)
+	{
+		*n *= -1;
+		*negative = 1;
+	}
+}
+
+char*ft_itoa(int n)
+{
+	int		tmpn;
+	int 	len;
+	int		negative;
+	char	*str;
+
+	if (n == -2147483648)					
+		return (ft_strdup("-2147483648"));
+	tmpn = n;
+	len = 2;
+	negative = 0;
+	itoa_isnegative(&n, &negative);
+	while (tmpn /= 10)
+		len++;
+	len += negative;
+	if ((str = (char*)malloc(sizeof(char) * len)) == NULL)
+		return (NULL);
+	str[--len] = '\0';
+	while (len--)
+	{
+		str[len] = n % 10 + '0';
+		n = n / 10;
+	}
+	if (negative)
+		str[0] = '-';
+	return (str);
 }
